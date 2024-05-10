486df182be9ac83505923c2ddfe35aec80c36a1f
Nandkishore Parmar <nandkishoreparmar97@gmail.com>
2024-05-10 21:07:18 +0530

    Input output practice questions

--- PR-101-B35/input/input.py
+++ PR-101-B35/input/input.py
@@ -1,15 +1,15 @@
 
 
 
-# num = input(" Enter a number ")
+num = input(" Enter a number ")
 
-# print(num)
-# print(type(num))   # str 
+print(num)
+print(type(num))   # str 
 
-# num1 = int(input())    # take input number without space 12    not 1 2 
-# print(num1)
-# print(type(num1))   # number 
+num1 = int(input())    # take input number without space 12    not 1 2 
+print(num1)
+print(type(num1))   # number 
 
-flo = float(input())
+flo = float(input())   # 11 1.2    not take 1 2 
 print(flo)
 print(type(flo))
---
+++ PR-101-B35/input/input2.py
@@ -0,0 +1,7 @@
+
+num1 = input()   #
+
+list1 = num1.strip().split(" ")# strip() function is used to remove spaces _1 2 3 4 5_   1 2 3 4 5  front and back  
+
+print(list1)
+print(type(list1))
