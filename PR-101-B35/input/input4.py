2848c4eec4471a818b88a2d49c29eeedaff95424
Nandkishore Parmar <nandkishoreparmar97@gmail.com>
2024-05-14 21:36:04 +0530

    edit

--- PR-101-B35/input/input4.py
+++ PR-101-B35/input/input4.py
@@ -7,7 +7,7 @@
 no_of_lines = int(input(" Enter no. of lines: "))
 
 lines = [input() for _ in range(no_of_lines)]
-size = lines[0]
+size = lines[0]   # first index of arr
 el = lines[1].strip().split(" ")
 ans = [int(num) for num in el]
 
