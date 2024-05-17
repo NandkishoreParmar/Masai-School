5b8b5fa2435953f65a7354ca2fbb6be1c77901b1
Nandkishore Parmar <nandkishoreparmar97@gmail.com>
2024-05-17 21:05:46 +0530

    add

--- Unit-1/PR-101-B35/input/inputDiffline12.py
+++ Unit-1/PR-101-B35/input/inputDiffline12.py
@@ -7,6 +7,14 @@
 
 # 2 nd way 
 
-Num = int(input())
-arr1 = list(map(int,input().split()))
-print(arr1)
+# Num = int(input())
+# arr1 = list(map(int,input().split()))
+# print(arr1)
+
+# 3   how to take input in 2D array
+
+m,n = map(int,input().split())
+arr = []
+for i in range(m):
+    arr.append(list(map(int,input().split())))
+print(arr)
