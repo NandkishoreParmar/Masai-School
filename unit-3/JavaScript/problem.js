35f074b7444ceb8694c094049c380ac1161fcb92
Nandkishore Parmar <nandkishoreparmar97@gmail.com>
2024-06-23 22:36:43 +0530

    add more

--- unit-3/JavaScript/problem.js
+++ unit-3/JavaScript/problem.js
@@ -1,6 +1,26 @@
 // console.log("Hello World")
 
-var str = ['q',1,2]
-str = String(str)
+// var str = ['q',1,2]
+// str = String(str)
 
-console.log(str, typeof str) 
+// console.log(str, typeof str) 
+// const str = "Thisisastring";
+// const words = str.split("This"); // Split by spaces
+
+// console.log(words); // Output: ["This", "is", "a", "string"]
+function splitAtWord(str, separator) {
+    if (str.startsWith(separator)) {
+      const remaining = str.slice(separator.length);
+      return [separator, remaining];
+    } else {
+      return [str]; // Return the original string if separator not found at the beginning
+    }
+  }
+  
+  const str = "helloworld";
+  const separator = "hello";
+  const splitResult = splitAtWord(str, separator);
+  
+  console.log(splitResult); // Output: ["hello", "world"] (if "hello" is at the beginning)
+                              //        or ["helloworld"] (if "hello" is not found)
+  
