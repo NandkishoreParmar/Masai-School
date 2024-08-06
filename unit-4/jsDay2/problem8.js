b8e4047b9584050ef4f9b03f618b34cfcd9ce362
Pankaj narvariya <nandkishoreparmar91@gmail.com>
2024-08-06 10:46:40 +0530

    total expenses

---
+++ unit-4/jsDay2/problem8.js
@@ -0,0 +1,17 @@
+// Function to calculate total expenses
+function calculateTotalExpenses(expenses) {
+    return expenses.reduce((total, expense) => total + expense.amount, 0);
+  }
+  
+  // Example usage
+  const expenses = [
+    { name: "Food", amount: 50 },
+    { name: "Transportation", amount: 30 },
+    { name: "Internet", amount: 45 },
+    { name: "Groceries", amount: 80 }
+  ];
+  
+  const totalExpenses = calculateTotalExpenses(expenses);
+  console.log(totalExpenses); // Output should be: 205
+  
+  
